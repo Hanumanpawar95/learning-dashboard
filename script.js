50b3a0e18e977d0e743dadb187b36a931714bc6d
Hanumanpawar95 <132887253+Hanumanpawar95@users.noreply.github.com>
2026-06-09 12:02:03 +0530

    some letter error fixed

--- script.js
+++ script.js
@@ -442,7 +442,7 @@ console.log(dashboardHTML);
 
     pdf.save("Batch_Report.pdf");
   }
-wwindow.showEligibleLearners = function(course){
+ window.showEligibleLearners = function(course){
 
 const learners =
 window.eligibleLearners?.[course] || [];
